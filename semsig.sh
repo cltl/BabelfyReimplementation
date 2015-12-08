fd0011a2fb908892110161e3d515f5294025e59c
Minh Le <ngocminh.oss@gmail.com>
2015-12-09 00:14:57 +0100

    Update semsig.sh

--- semsig.sh
+++ semsig.sh
@@ -4,4 +4,4 @@ DBPATH=/home/fii800/BabelNet/semsig_files/mongodb
 
 mongod --quiet --dbpath $DBPATH > /dev/null 2>&1 &
 sleep 10
-python semsig.py phase3 -y 2>&1 | tee semsig.out
+python semsig.py $1 -y 2>&1 | tee semsig.out
