44071401c91a9bf6905d66ca4413cecd102fb988
Minh Le <ngocminh.oss@gmail.com>
2015-12-09 00:10:09 +0100

    Update genrel.sh

--- genrel.sh
+++ genrel.sh
@@ -4,7 +4,9 @@ BLOOKUP_HOME=/scratch/fii800/BabelNet/babelnet-lookup
 cd $BLOOKUP_HOME
 mvn clean package
 cd $BABELNET_HOME
-#java -cp lib/*:$BLOOKUP_HOME:$BLOOKUP_HOME/target/lib/*:$BLOOKUP_HOME/target/babelnet-lookup-0.0.1-SNAPSHOT.jar spinoza.util.TripletGenerator -r -from $FROM > $OUTPUT
+
+# generate a text file with all triples
+java -cp lib/*:$BLOOKUP_HOME:$BLOOKUP_HOME/target/lib/*:$BLOOKUP_HOME/target/babelnet-lookup-0.0.1-SNAPSHOT.jar spinoza.util.TripletGenerator -r -from $FROM > $OUTPUT
 
 # to build name collection
 java -cp lib/*:$BLOOKUP_HOME:$BLOOKUP_HOME/target/lib/*:$BLOOKUP_HOME/target/babelnet-lookup-0.0.1-SNAPSHOT.jar spinoza.util.BabelNet2MongoDB
